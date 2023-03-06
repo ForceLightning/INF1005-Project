b93f841da29bb148bcaa0416276aafab2a54a23e
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-03-07 07:15:24 +0800

    add placeholder files

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/main.css">
         <!-- JQuery JS -->
         <script defer src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
         <!-- Bootstrap JS -->
