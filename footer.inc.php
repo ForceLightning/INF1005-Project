c775057030c87ef0407a8ed33c6148baa6bc7246
Jullisha <jullisha.sasikumar@gmail.com>
2023-03-11 20:17:49 +0800

    footer and navbar
    footer and navbar design

--- footer.inc.php
+++ footer.inc.php
@@ -6,36 +6,37 @@
     <?php echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">' ?>
 </head>
 <body>
-    <footer class="bg-light py-3">
+    <footer class="bg-dark py-3">
 	<div class="container">
             <div class="row">
 		<div class="col-md-4">
-                    <h5 class="mb-3">Navigation</h5>
+                    <h5 class="mb-3 text-white">Navigation</h5>
 			<ul class="list-unstyled">
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="aboutUs.php">About Us</a></li>
-                            <li><a href="#">Facilities</a></li>
-                            <li><a href="#">Events</a></li>
+                            <li><a href="index.php" class="text-white">Home</a></li>
+                            <li><a href="aboutUs.php" class="text-white">About Us</a></li>
+                            <li><a href="#" class="text-white">Facilities</a></li>
+                            <li><a href="#" class="text-white">Events</a></li>
 			</ul>
 		</div>
 		<div class="col-md-4">
-                    <h5 class="mb-3">Connect with us</h5>
+                    <h5 class="mb-3 text-white">Connect with us</h5>
 			<ul class="list-unstyled list-inline">
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white"><i class="fab fa-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white"><i class="fab fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white"><i class="fab fa-instagram"></i></a></li>
 			</ul>
+                    
 		</div>
 		<div class="col-md-4">
-                    <h5 class="mb-3">Contact Us</h5>
-			<p>444 Mandai Road, Singapore 779564</p>
-			<p>Phone: 6457 9874</p>
-			<p>Email: mandai@cc.com</p>
+                    <h5 class="mb-3 text-white">Contact Us</h5>
+			<p class=" mb-3 text-white">444 Mandai Road, Singapore 779564</p>
+			<p class=" mb-3 text-white">Phone: 6457 9874</p>
+			<p class=" mb-3 text-white">Email: mandai@cc.com</p>
 		</div>
             </div>
             <div class="row mt-3">
-		<div class="col-md-12">
-                    <p class="text-center">&copy; 2023 Mandai CC. All rights reserved.</p>
+		<div class="col-md-12" class="text-white">
+                    <p class="text-center mb-3 text-white">&copy; 2023 Mandai CC. All rights reserved.</p>
 		</div>
             </div>
         </div>
--- nav.inc.php
+++ nav.inc.php
@@ -1,9 +1,11 @@
 <!-- TODO: Write navbar here -->
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
-        <img src='images/petshop.png' alt="Logo" width="50">
+        <img src='assets/Logo.png' alt="Logo" width="50">
     </a>
-    <ul class="navbar-nav">
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
         <li class="nav-item">
             <a class="nav-link" href="index.php">Home</a>
         </li>
@@ -16,11 +18,26 @@
         <li class="nav-item">
             <a class="nav-link" href="#Events">Events</a>
         </li>
-        <li class="nav-item">
+<!--        <li class="nav-item">
             <a class="nav-link" href="register.php">Register</a>
-        </li>
-        <li class="nav-item">
+        </li>-->
+    </ul>
+        
+    <ul class="navbar-nav navbar-right">
+           <li class="nav-item">
+               <a class="nav-link" href="login.php">
+                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                   <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                   </svg>
+               </a>
+           </li>
+       </ul>
+    </div>
+
+        
+<!--        <li class="nav-item">
             <a class="nav-link" href="login.php">Login</a>
         </li>
-    </ul>
+    </ul>-->
 </nav>
