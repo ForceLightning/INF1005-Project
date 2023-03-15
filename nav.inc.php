702468d88077aef72ad4cbb3c4d7d5b2f7a46c3c
cherylthy <59245154+cherylthy@users.noreply.github.com>
2023-03-15 09:45:37 +0800

    include carousel and make a booking on nav

--- nav.inc.php
+++ nav.inc.php
@@ -20,7 +20,10 @@
             <a class="nav-link" href="facilities.php">FACILITIES</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#Events">EVENTS</a>
+            <a class="nav-link" href="events.php">EVENTS</a>
+        </li>
+        <li class ="nav-item">
+            <a class="nav-link" href="#Bookings">Make a Booking</a>
         </li>
 <!--        <li class="nav-item">
             <a class="nav-link" href="register.php">Register</a>
