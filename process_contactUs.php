f7e5c832bcc574f2e5c4261786a52e09703abaa4
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-03-25 12:36:39 +0800

    fix: forgot to include autoload.php

--- process_contactUs.php
+++ process_contactUs.php
@@ -2,27 +2,7 @@
 <html>
 
 <head>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/main.css">
-    <!--Google fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Kavoon&family=Poppins:wght@300&family=Quicksand:wght@300&family=Rubik&display=swap"
-        rel="stylesheet">
-    <!-- JQuery JS -->
-    <script defer src="https://code.jquery.com/jquery-3.6.3.min.js"
-        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    <!-- Bootstrap JS -->
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-        crossorigin="anonymous"></script>
-    <!-- Custom JS -->
-    <!-- TODO: Comment this out after writing main.js -->
-    <script defer src="js/main.js"></script>
+    <?php include "includes.php"; ?>
     <title>Contact Us</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,11 +12,13 @@
 $fname = $lname = $email = $msg = $errorMsg = "";
 $success = true;
 
+require "vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $branch = $_ENV['BRANCH'];
 
-function save_contact_form_to_db() {
+function save_contact_form_to_db()
+{
     global $fname, $lname, $email, $message, $errorMsg, $success, $branch;
     $config = parse_ini_file('../../private/project-db-config.ini', true);
     $conn = new mysqli($config[$branch]['servername'], $config[$branch]['username'], $config[$branch]['password'], $config[$branch]['dbname']);
@@ -55,7 +37,7 @@ function save_contact_form_to_db() {
         $stmt->close();
     }
     $conn->close();
-    
+
 }
 
 # Process only if the form has been submitted via POST
@@ -93,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message = sanitize_input($_POST["message"]);
     }
 } else {
-    echo"<h2>This page is not meant to run directly.<h2>";
+    echo "<h2>This page is not meant to run directly.<h2>";
     exit();
 }
 
@@ -116,7 +98,7 @@ include "nav.inc.php"
     <hr>
     <?php
     if ($success) {
-        if(!empty($fname)) {
+        if (!empty($fname)) {
             $name = $fname . " " . $lname;
         } else {
             $name = $lname;
