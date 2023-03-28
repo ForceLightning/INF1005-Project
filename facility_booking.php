29d4d1b612ce4132e3f8817c7a85ef5e2913b278
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-03-28 13:33:16 +0800

    feat: added booking buttons programmatically

--- facility_booking.php
+++ facility_booking.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="css/facility_booking.css">
     <?php include_once "includes/header.inc.php"; ?>
     <!-- custom JS, in local files -->
-    <script defer src="js/facility_booking.js"></script>
+    <script src="js/facility_booking.js"></script>
     <!-- this line will auto func the document ready function in js-->
     <title>Bookings</title>
     <meta charset="utf-8">
@@ -23,73 +23,12 @@
     <main class="container">
         <section>
             <!-- facility portion -->
-            <div class="facility-row row">
-                <div class="facility-card col-lg-4 col-md-6 mb-3" facility-name="Facility 1">
-                    <div class="text-black-50">
-                        <div class="row">
-                            <div>
-                                <!--<img src="" class="">-->
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 mb-0">
-                                <div>Facility 1</div>
-                                <hr>
-                                <div class="small">
-                                    Facility 1 Description
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="facility-card col-lg-4 col-md-6 mb-3" facility-name="Facility 2">
-                    <div class="text-black-50">
-                        <div class="row">
-                            <div>
-                                <!--<img src="" class="">-->
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 mb-0">
-                                <div>Facility 2</div>
-                                <hr>
-                                <div class="small">
-                                    Facility 2 Description
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="facility-row row" id="facility-cards">
             </div>
         </section>
         <!-- timeslot portion  -->
-        <section>
+        <section id="timeslots">
             <div class="timeslot-row row">
-                <div class="timeslot-group col-xl-4 col-lg-6 col-sm-6 col-xs-6 mb-2 ">
-                    <div name="timeslot-white"
-                        class="timeslot-white d-sm-inline-block btn btn-sm bg-avaiable-status w-100"
-                        data-timeslot="1200">
-                        12:00 - 13:00
-                    </div>
-                </div>
-                <div class="timeslot-group col-xl-4 col-lg-6 col-sm-6 col-xs-6 mb-2 ">
-                    <div name="timeslot-white"
-                        class="timeslot-white d-sm-inline-block btn btn-sm bg-avaiable-status w-100"
-                        data-timeslot="1300">
-                        13:00 - 14:00
-                    </div>
-                </div>
-                <div class="timeslot-group col-xl-4 col-lg-6 col-sm-6 col-xs-6 mb-2 ">
-                    <div name="timeslot-white"
-                        class="timeslot-white d-sm-inline-block btn btn-sm bg-avaiable-status w-100"
-                        data-timeslot="1400">
-                        14:00 - 15:00
-                    </div>
-                </div>
-                <div class="timeslot-group col-xl-4 col-lg-6 col-sm-6 col-xs-6 mb-2 ">
-                    <div name="timeslot-white"
-                        class="timeslot-white d-sm-inline-block btn btn-sm bg-avaiable-status w-100"
-                        data-timeslot="1500">
-                        15:00 - 16:00
-                    </div>
-                </div>
             </div>
 
             <form id="booking-form" action="process_facility_booking.php" method="post" id="" novalidate>
--- js/facility_booking.js
+++ js/facility_booking.js
@@ -15,23 +15,93 @@ $(document).ready(function () {
     const bookingSlots = getBookingSlots(true);
     // TODO: Iterate through the dict that maps location_id => timeslots
     // and add the timeslots to the corresponding facility card
+
+
+    const form = document.querySelector('#booking-form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // prevent the form from submitting normally
+        const formData = new FormData(form); // create a FormData object from the form
+        formData.append('facility', selectedFacility); // add the selectedFacility value to the form data
+        formData.append('timeslot', JSON.stringify(selectedTimeSlots)); // add the selectedTimeSlots array to the form data
+        fetch('process_facility_booking.php', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Form data submitted successfully');
+                    // do something here, such as redirect to a thank you page
+                    window.location.href = 'process_facility_booking.php';
+                } else {
+                    console.error('Error submitting form data');
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    });
+
+});
+
+function getBookingSlots(debug) {
+    const url = debug ? "api/bookings_api.json" : "api/bookings_api.php";
+    let bookingSlots = {};
+    $.get(url, function (data) {
+        bookingSlots = data;
+        console.log(bookingSlots);
+        displayBookingSlots(bookingSlots);
+        //getting all facility elements 
+        const facilityElements = document.querySelectorAll('.facility-card');
+        //getting all timeslot elements
+        const timeSlotElements = document.querySelectorAll('.timeslot-white');
+        addListeners(facilityElements, timeSlotElements);
+    });
+    return bookingSlots;
+}
+
+function displayBookingSlots(bookingSlots) {
     for (let location_id of Object.keys(bookingSlots)) {
         // Get the timeslots for this location
         let timeslots = bookingSlots[location_id];
-        // Get the facility card for this location
-        let facilityCard = document.getElementById(location_id);
+        // Create a facility card for the location
+        let facilityCard = document.createElement("div");
+        facilityCard.classList.add("facility-card", "col-lg-4", "col-md-6", "mb-3");
+        facilityCard.setAttribute("facility-name", bookingSlots[location_id][0]["location_name"]);
+        facilityCard.innerHTML = `
+            <div class="text-black-50">
+            <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 mb-0">
+            <div>` + bookingSlots[location_id][0]["location_name"] + `</div>
+            <hr>
+            </div>
+            </div>
+            </div>
+        `;
+        let timeslotContainer = document.createElement("div");
+        timeslotContainer.classList.add("timeslot-group", "col-xl-4", "col-lg-6", "col-sm-6", "col-xs-6", "mb-2");
+        timeslotContainer.setAttribute("facility-id", location_id);
         // Add the timeslots to the facility card
         for (let timeslot of timeslots) {
             // Create a new timeslot element
             let timeslotElement = document.createElement("div");
-            timeslotElement.classList.add("timeslot-white");
-            timeslotElement.setAttribute("data-timeslot", timeslot);
-            timeslotElement.innerHTML = timeslot;
+            timeslotElement.classList.add("timeslot-white", "d-sm-inline-block", "btn", "btn-sm", "bg-available-status", "w-100");
+            let startTime = timeslot["time_start"].split(/[- :]/);
+            var start = new Date(startTime[0], startTime[1]-1, startTime[2], startTime[3], startTime[4], startTime[5]);
+            let endTime = timeslot["time_end"].split(/[- :]/);
+            var end = new Date(endTime[0], endTime[1]-1, endTime[2], endTime[3], endTime[4], endTime[5]);
+            var timeslotText = start.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + " - " + end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+            timeslotElement.setAttribute("booking-id", timeslot["booking_id"]);
+            timeslotElement.innerHTML = timeslotText;
             // Add the timeslot element to the facility card
-            facilityCard.appendChild(timeslotElement);
+            timeslotContainer.appendChild(timeslotElement);
         }
+        // add the facility card to the page
+        document.getElementById("facility-cards").appendChild(facilityCard);
+        document.getElementsByClassName("timeslot-row")[0].appendChild(timeslotContainer);
     }
+}
 
+function addListeners(facilityElements, timeSlotElements) {
     //add event listeners to the facility cards
     facilityElements.forEach((element) => {
         //element.classList.toggle('selected');
@@ -82,39 +152,4 @@ $(document).ready(function () {
             //console.log(selectedTimeSlots); // test
         });
     });
-
-    const form = document.querySelector('#booking-form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // prevent the form from submitting normally
-        const formData = new FormData(form); // create a FormData object from the form
-        formData.append('facility', selectedFacility); // add the selectedFacility value to the form data
-        formData.append('timeslot', JSON.stringify(selectedTimeSlots)); // add the selectedTimeSlots array to the form data
-        fetch('process_facility_booking.php', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => {
-                if (response.ok) {
-                    console.log('Form data submitted successfully');
-                    // do something here, such as redirect to a thank you page
-                    window.location.href = 'process_facility_booking.php';
-                } else {
-                    console.error('Error submitting form data');
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    });
-
-});
-
-function getBookingSlots(debug) {
-    const url = debug ? "api/bookings_api.json" : "api/bookings_api.php";
-    let bookingSlots = {};
-    $.get(url, function (data) {
-        bookingSlots = data;
-        console.log(bookingSlots);
-    });
-    return bookingSlots;
 }
